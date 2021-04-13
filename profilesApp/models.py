e0b7886b1e89427b49aafb98b43295ee3b43c970
Waseem Khan <mdvaseem014@gmail.com>
2021-04-13 11:45:19 +0530

    created Relationship model and created signal

--- profilesApp/models.py
+++ profilesApp/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from .utils import get_random_code
 from django.template.defaultfilters import slugify
 
+
 # Create your models here.
 
 class Profile(models.Model):
@@ -22,17 +23,34 @@ class Profile(models.Model):
     created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.user.username}-{self.created}"
+        return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')}"
 
-    def save(self, *args,**kwargs):
+    def save(self, *args, **kwargs):
         ex = False
         if self.first_name and self.last_name:
             to_slug = slugify(str(self.first_name) + " " + str(self.last_name))
             ex = Profile.objects.filter(slug=to_slug).exists()
             while ex:
-                to_slug=slugify(to_slug + " " + str(get_random_code()))
+                to_slug = slugify(to_slug + " " + str(get_random_code()))
                 ex = Profile.objects.filter(slug=to_slug).exists()
         else:
             to_slug = str(self.user)
         self.slug = to_slug
         super().save(*args, **kwargs)
+
+
+STATUS_CHOICE = (
+    ('send', 'send'),
+    ('accepted', 'accepted'),
+)
+
+
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.sender}-{self.sender}-{self.status}"
