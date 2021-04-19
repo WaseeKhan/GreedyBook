d44e2bc95b2fc6faaf333cccb91895f9110f8182
Waseem Khan <mdvaseem014@gmail.com>
2021-04-19 12:57:39 +0530

    Coomet and post section added

--- postsApp/views.py
+++ postsApp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Post, Like
 from profilesApp.models import Profile
+from .forms import PostModelForm, CommentModelForm
 
 
 # Create your views here.
@@ -8,7 +9,37 @@ from profilesApp.models import Profile
 def post_comment_create_list_view(request):
     qs = Post.objects.all()
     profile = Profile.objects.get(user=request.user)
-    context = {'qs': qs,'profile':profile, }
+
+    p_form = PostModelForm()
+    c_form = CommentModelForm()
+    post_added = False
+
+    profile = Profile.objects.get(user=request.user)
+
+    if 'submit_p_form' in request.POST:
+        print(request.POST)
+        p_form = PostModelForm(request.POST, request.FILES)
+        if p_form.is_valid():
+            instance = p_form.save(commit=False)
+            instance.author = profile
+            instance.save()
+            p_form = PostModelForm()
+            post_added = True
+    if 'submit_c_form' in request.POST:
+        c_form = CommentModelForm(request.POST)
+        if c_form.is_valid():
+            instance = c_form.save(commit=False)
+            instance.user = profile
+            instance.post = Post.objects.get(id=request.POST.get('post_id'))
+            instance.save()
+            c_form = CommentModelForm()
+    context = {
+        'qs': qs,
+        'profile': profile,
+        'p_form': p_form,
+        'c_form': c_form,
+        'post_added':post_added,
+    }
 
     return render(request, 'postsApp/main.html', context)
 
@@ -26,8 +57,8 @@ def like_unlike_post(request):
             post_obj.liked.add(profile)
         like, created = Like.objects.get_or_create(user=profile, post_id=post_id)
         if not created:
-            if like.value=='Like':
-                like.value='Unlike'
+            if like.value == 'Like':
+                like.value = 'Unlike'
             else:
                 like.value = 'Like'
             post_obj.save()
