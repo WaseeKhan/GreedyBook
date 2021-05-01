12f1c5431814578b194ce812d905689e7bcc8703
Waseem Khan <mdvaseem014@gmail.com>
2021-05-01 12:12:10 +0530

    Add, remove, send invitation etc added

--- profilesApp/models.py
+++ profilesApp/models.py
@@ -2,9 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 from .utils import get_random_code
 from django.template.defaultfilters import slugify
+from django.db.models import Q
 
 
 # Create your models here.
+
+class ProfileManager(models.Manager):
+    def get_all_profiles_to_invite(self, sender):
+        profiles = Profile.objects.all().exclude(user=sender)
+        profile = Profile.objects.get(user=sender)
+        qs = Relationship.objects.filter(Q(sender=profile) | Q(receiver=profile))
+        print(qs)
+        print("======================")
+        accepted = []
+        for rel in qs:
+            if rel.status == 'accepted':
+                accepted.append(rel.receiver)
+                accepted.append(rel.sender)
+        print(accepted)
+        print("-------------------")
+        available = [profile for profile in profiles if profile not in accepted]
+        print(available)
+        print("******************")
+        return available
+
+    def get_all_profiles(self, me):
+        profiles = Profile.objects.all().exclude(user=me)
+        return profiles
+
 
 class Profile(models.Model):
     first_name = models.CharField(max_length=200, blank=True)
@@ -21,11 +46,15 @@ class Profile(models.Model):
     slug = models.SlugField(unique=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now=True)
+
+    objects = ProfileManager()
+
     def get_friends(self):
         return self.friends.all()
 
     def get_friends_no(self):
         return self.friends.all().count()
+
     def get_posts_no(self):
         return self.posts.all().count()
 
@@ -34,9 +63,9 @@ class Profile(models.Model):
 
     def get_likes_given_no(self):
         likes = self.like_set.all()
-        total_liked= 0
+        total_liked = 0
         for item in likes:
-            if item.value=='Like':
+            if item.value == 'Like':
                 total_liked += 1
         return total_liked
 
@@ -44,7 +73,7 @@ class Profile(models.Model):
         posts = self.posts.all()
         total_liked = 0
         for item in posts:
-            total_liked +=item.liked.all().count()
+            total_liked += item.liked.all().count()
         return total_liked
 
     def __str__(self):
@@ -70,6 +99,12 @@ STATUS_CHOICE = (
 )
 
 
+class RelationshipManager(models.Manager):
+    def invatations_received(self, receiver):
+        qs = Relationship.objects.filter(receiver=receiver, status='send')
+        return qs
+
+
 class Relationship(models.Model):
     sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
     receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
@@ -77,5 +112,7 @@ class Relationship(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now=True)
 
+    objects = RelationshipManager()
+
     def __str__(self):
-        return f"{self.sender}-{self.sender}-{self.status}"
+        return f"{self.sender}-{self.receiver}-{self.status}"
--- profilesApp/urls.py
+++ profilesApp/urls.py
@@ -1,11 +1,14 @@
 
-
 from django.urls import path
-from .views import my_profile_view
+from .views import remove_from_friends, send_invitation,my_profile_view, invites_received_view,profiles_list_view, invite_profiles_list_view, ProfilelistView
 
 app_name = 'profilesApp'
 
 urlpatterns = [
-    path('myprofile/',my_profile_view, name='my-profile-view'),
-
+    path('myprofile/' ,my_profile_view, name='my-profile-view'),
+    path('my-invites/', invites_received_view, name = 'my-invites-view'),
+    path('all-profiles/', ProfilelistView.as_view(), name = 'all-profiles-view'),
+    path('to-invite/', invite_profiles_list_view, name = 'invite-profiles-view'),
+    path('send-invites/', send_invitation, name = 'send-invite'),
+    path('remove-friend/', remove_from_friends, name = 'remove-friend'),
 ]
