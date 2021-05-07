56666141a4ed1d168dabffbe717ed9807b0cf9be
Waseem Khan <mdvaseem014@gmail.com>
2021-05-07 18:29:35 +0530

    v1.0 PRD

--- GreedyBookProject/settings.py
+++ GreedyBookProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -164,3 +165,5 @@ MEDIA_ROOT = os.path.join(os.path.join(BASE_DIR), "static_cdn", "media_root")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
