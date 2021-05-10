be70683449dbb7ae5713fbc05539fce8e9cb35b1
Waseem Khan <mdvaseem014@gmail.com>
2021-05-10 16:43:51 +0530

    simpleLazyObject issue

--- profilesApp/views.py
+++ profilesApp/views.py
@@ -93,8 +93,8 @@ class ProfileDetailView(LoginRequiredMixin, DetailView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         # context["Hello"] = "Hello World"
-        user = User.objects.get(username__iexact=self.request.user)
-        profile = Profile.objects.get(user=user)
+        #user = User.objects.get(username__iexact=self.request.user)
+        profile = Profile.objects.get(user=self.request.user)
         context['profile'] = profile
         rel_r = Relationship.objects.filter(sender=profile)
         rel_s = Relationship.objects.filter(receiver=profile)
